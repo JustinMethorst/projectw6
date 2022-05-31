fb0ce6a317f3ae448597cf68ae4687c2656f4244
MakerslabEde <methorstjustin@gmail.com>
2022-05-31 15:57:59 +0200

    Init commit

--- app/Models/Afspraak.php
+++ app/Models/Afspraak.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Afspraak extends Model
 {
     use HasFactory;
+
+    protected $table = 'afspraak';
+
+    public const CREATED_AT = null;
+    public const UPDATED_AT = null;
+
+    protected $primaryKey = 'afspraak_id';
+
+    protected $afspraak_bevestiging = 'tinyint';
+    protected $datum_afspraak = 'date';
+    protected $tijd_afspraak = 'time';
+    protected $afspraak_opmerking = 'text';
+    protected $afspraak_status = 'enum';
+    protected $afspraak_type = 'enum';
+    protected $monteur_id = 'int';
+    protected $klant_id = 'int';
+    protected $klant_opmerking = 'text';
+    protected $factuur_betaald = 'bool';
+
 }
